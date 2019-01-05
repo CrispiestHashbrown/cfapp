1e27b35c966e93afbec1902a0421f54df471abaf
RK <r_kieu@outlook.com>
2019-01-05 04:35:14 -0800

    Tabs Page
    Added four tab icons for Search, Starred, Followed, Repos
    Added a tab highlight bar to indicate which tab is in focus

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+
+  tab1Root: string;
+  tab2Root: string;
+  tab3Root: string;
+  tab4Root: string;
+
+  constructor() {
+    this.tab1Root = 'SearchPage';
+    this.tab2Root = 'StarredPage';
+    this.tab3Root = 'FollowedPage';
+    this.tab4Root = 'ReposPage';
+  }
+
+}
